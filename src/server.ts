5642798cf4924937f0e7b956dd36f084150a9d21
Yuri Luiz <yuri.luitzkt@gmail.com>
2024-07-18 14:45:17 -0300

    chore: Update CORS configuration to allow requests from specific origins

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,12 @@ import { reportFeedbackRoutes } from './routes/report-feedback.routes';
 
 const app: FastifyInstance = fastify({ logger: true });
 
-app.register(cors, {
-    origin: '*',
+const corsOptions = {
+    origin: ['http://localhost:5173', process.env.FRONTEND_URL],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-});
+};
+
+app.register(cors, corsOptions);
 
 app.register(reportFeedbackRoutes, {
     prefix: '/report-feedback'
